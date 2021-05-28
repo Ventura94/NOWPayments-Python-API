36c358530c12dea3b8e142b619823f8102c7bb5f
Ventura <arianventura94@gmail.com>
2021-05-28 16:51:21 -0400

    Todo Listo

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nowpayments-python",
+    version="0.0.1",
+    author="Example Author",
+    author_email="arianventura94@gmail.com",
+    description="NOWPayments python API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ventura94/NOWPayments-Python-API",
+    project_urls={
+        "Bug Tracker": "https://github.com/Ventura94/NOWPayments-Python-API",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
