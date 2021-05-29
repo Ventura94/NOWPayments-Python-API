ba3ed4d216a0c328f064bd348aea83115172f8e8
Ventura <arianventura94@gmail.com>
2021-05-29 04:13:32 -0400

    Quitando lo que no debìa dejar

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nowpayments",
-    version="0.0.2",
+    version="0.0.3",
     author="Arian Ventura Rodrìguez",
     author_email="arianventura94@gmail.com",
     description="NOWPayments python API",
