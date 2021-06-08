41d43fc0c1df7919aae9abed8f4925ad170adb21
ventura94 <arianventura94@gmail.com>
2021-06-08 08:25:35 -0400

    0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nowpayments",
-    version="0.0.6",
+    version="0.0.7",
     author="Arian Ventura Rodrìguez",
     author_email="arianventura94@gmail.com",
     description="NOWPayments python API",
